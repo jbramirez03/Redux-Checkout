ef7417ad01a45c1206cb276f394efab2f533b8da
temptado <jason1287712@gmail.com>
2021-11-01 18:19:48 -0400

    refactored use of UseStoreContext with useDispatch and useSelector

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
@@ -10,7 +9,8 @@ import { useSelector, useDispatch } from 'react-redux';
 
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
 
   const { currentCategory } = state;
 
