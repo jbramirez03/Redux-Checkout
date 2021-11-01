57ea08abffbf42606e8cf2fd4f2047c81fec2f6e
temptado <jason1287712@gmail.com>
2021-11-01 18:20:49 -0400

    refactored use of useStoreContext with useDispatch and useSelector from redux library

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,7 +3,6 @@ import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -17,7 +16,8 @@ import { useSelector, useDispatch } from 'react-redux';
 
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
